2c807e20b31b2c2f4bc13d67a7cee1ec06f11e0f
HuuBng <89332259+HuuBng@users.noreply.github.com>
2025-05-28 17:55:25 +0700

    commit

---
+++ src/DUPSS/Components/DAL/CategoryDAO.cs
@@ -0,0 +1,23 @@
+﻿using Objects;
+
+namespace DAL
+{
+    public class CategoryDAO
+    {
+        public List<Category> GetCategories()
+        {
+
+            var listCategories = new List<Category>();
+            try
+            {
+                using var context = new MyStoreContext();
+                listCategories = context.Categories.ToList();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return listCategories;
+        }
+    }
+}

e82eb4c45f6a89d29216d8c597907b868fa372d2
phucpinku <hongphuchong456@gmail.com>
2025-06-10 12:32:34 +0700

    update CourseManagement
    Working on CourseManagement

--- src/DUPSS.Web/Program.cs
+++ src/DUPSS.Web/Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddScoped<CourseApiService>();
-builder.Services.AddHttpClient<CourseApiService>();
+
 builder.Services.AddHttpClient<CourseApiService>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:7288/");

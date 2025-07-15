a5dd452fe006dedd2b533ffb5795699f914b45f4
nahanwh <von2441@gmail.com>
2025-07-15 16:55:20 +0700

    export function in dashboard

---
+++ src/DUPSS.Web/wwwroot/js/exportExcel.js
@@ -0,0 +1,7 @@
+﻿window.exportDashboardToExcel = (filename, rows) => {
+    // rows: array of arrays, first row is header
+    var ws = XLSX.utils.aoa_to_sheet(rows);
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Dashboard");
+    XLSX.writeFile(wb, filename);
+};
